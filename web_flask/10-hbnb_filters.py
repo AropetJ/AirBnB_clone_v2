43aafdf117aa4476f596022cbc4d3b615290e47e
AropetJ <aropetjoel32@gmail.com>
2023-10-26 23:25:11 +0300

    Attempted task 8

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -10,5 +10,21 @@ from models import *
 app = Flask(__name__)
 
 
+
+@app.teardown_appcontext
+def tearDown(exception):
+    """Closes the storage engine on teardown"""
+    storage.close()
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def filters():
+    """Passes states and cities sorted by name and amenities"""
+    states = storage.all("State").values()
+    amenities = storage.all("Amenity").values()
+    return render_template('10-hbnb_filters.html', states=states,
+                           amenities=amenities)
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
