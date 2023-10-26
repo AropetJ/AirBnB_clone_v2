f2c1ac5cc4b81eee2c4a8c52c511f69dfe87e5c6
AropetJ <aropetjoel32@gmail.com>
2023-10-26 23:27:08 +0300

    Attempted task 100

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -10,5 +10,35 @@ from models import *
 app = Flask(__name__)
 
 
+
+@app.teardown_appcontext
+def tearDown(exception):
+    """Closes the storage engine on teardown"""
+    storage.close()
+
+
+@pp.route("/hbnb", strict_slashes=False)
+def states_cities_list():
+    """pass states and cities sorted by name and amenities
+    """
+    states = list(storage.all("State").values())
+    states.sort(key=lambda x: x.name)
+    for state in states:
+        state.cities.sort(key=lambda x: x.name)
+    amenities = list(storage.all("Amenity").values())
+    amenities.sort(key=lambda x: x.name)
+    places = list(storage.all("Place").values())
+    places.sort(key=lambda x: x.name)
+    for place in places:
+        place.description = Markup(place.description)
+    return render_template(
+        '100-hbnb.html',
+        states=states,
+        amenities=amenities,
+        places=places
+    )
+
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
