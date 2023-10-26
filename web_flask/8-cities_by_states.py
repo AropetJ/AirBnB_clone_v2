596d8e511a54831ae14c6368df51b7dd5de2fa17
AropetJ <aropetjoel32@gmail.com>
2023-10-26 23:01:28 +0300

    Attempted task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,5 +10,21 @@ from models import *
 app = Flask(__name__)
 
 
+@app.route("/cities_by_states", strict_slashes=False)
+def states_cities_list():
+    """Lists states and cities sorted by name"""
+    states = list(storage.all("State").values())
+    states.sort(key=lambda x: x.name)
+    for state in states:
+        state.cities.sort(key=lambda x: x.name)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    """reload storage after each request
+    """
+    storage.close()
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
